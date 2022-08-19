48343bb3c422fc7b0d90379d186d615b6165122d
Dan <vatolin_denis@mail.ru>
2022-08-20 00:59:56 +0700

    seminar start

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿/*
+// Задача 1. Текст задачи.
+
+Console.Write("Input integer number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int kvadrat = number * number;
+
+Console.WriteLine($"Quad of {number} is {kvadrat}");
+*/
+
+// Задача 2. Напишите программу , которая на вход принимает два числа и проверяет, является ли первое число квадратом второго числа?
+
+/*
+Console.Write("Input first integer number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input second integer number: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1==num2  * num2)
+{
+ Console.WriteLine($"{num1} is a quad of {num2}");   
+}
+else
+ {
+  Console.WriteLine($"{num1} is NOT a  quad of {num2}");  
+ }
+
+*/
+
+// Задача 3. Напишите программу , которая на вход принимает одно число (N), а на выходе показывает все целые числа в промежутке от -N до N.
+
+/*
+Console.Write("Input pozitive integer number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = num * (-1);
+while(current <= num)
+{
+Console.Write(current + " ");
+current++;
+
+}
+*/
+
